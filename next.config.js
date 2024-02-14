2dad9be4d47cef2afc812e8cd8bfad8c99e34a8a
Geeleed <surasak.kaewpho@gmail.com>
2024-02-14 22:15:09 +0700

    use "@ducanh2912/next-pw"

--- next.config.js
+++ next.config.js
@@ -3,19 +3,39 @@
 
 // export default nextConfig;
 
-// next.config.js
-/** @type {import('next').NextConfig} */
+// // next.config.js
+// /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa")({
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+//   disable: process.env.NODE_ENV === "development",
+// });
+
+// const nextConfig = withPWA({
+//   reactStrictMode: true,
+//   swcMinify: true,
+// });
+
+// module.exports = nextConfig;
+
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
-const nextConfig = withPWA({
-  reactStrictMode: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
   swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
 });
 
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... other options you like
+};
+
+module.exports = withPWA(nextConfig);
